c99c6dde7c18d3c84d165ac055118a1f32130408
tamireinhorn <tamtamireinhorn@gmail.com>
2022-05-23 00:30:36 +0300

    Improving Rest Api,.py

--- rest-api/rest_api.py
+++ rest-api/rest_api.py
@@ -1,9 +1,11 @@
-from copy import copy
+from copy import deepcopy
 import json 
 from operator import itemgetter
 
+
 def _clean_url(url: str) -> str :
     return url.removeprefix('/')
+
 
 def get_keys(dict: dict):
     return list(map(itemgetter(0), dict.items()))
@@ -45,15 +47,12 @@ class RestAPI:
         if url == 'iou':
             return self.__add_iou(payload)
         raise ValueError('Incorrect request passed to API. Valid POST methods are add, iou.')
-            
-    def __get_user(self, user_name: str) -> str:
-        return self.database.get(user_name)
 
     def __add_user(self, payload: str) -> str:
         new_user = payload['user']
         new_user_entry = {'owes': {}, 'balance': 0.0}
         self.database[new_user] = new_user_entry
-        return_new_user_entry = copy(new_user_entry)
+        return_new_user_entry = deepcopy(new_user_entry)
         return_new_user_entry['owed_by'] = {}
         return_new_user_entry['name'] = new_user
         return json.dumps(return_new_user_entry)
@@ -65,26 +64,22 @@ class RestAPI:
         amount = payload['amount']
         lender_entry = self.__modify_user(lender, borrower, -amount)
         borrower_entry = self.__modify_user(borrower, lender, amount)
-        
         return json.dumps({'users': sorted([lender_entry, borrower_entry], key = lambda x: x['name'])}) # The expected return is ordered by the name of the users in the IOU.   
 
     def parse_database_return(self, user: str) -> dict: 
         # This method exists because the output expects two dicts, one of owes and other of owed_by
-        copy_user_entry = copy(self.__get_user(user))
+        copy_user_entry = deepcopy(self.database.get(user))
         copy_user_entry['owed_by'] = {user: -amount for user, amount in copy_user_entry['owes'].items() if amount < 0} # Negative debt -> user is OWED BY, goes to new dict for output.
         copy_user_entry['owes'] = {user: amount for user, amount in copy_user_entry['owes'].items() if amount > 0} # Positive debt in owes -> stays
         copy_user_entry['name'] = user
         return copy_user_entry 
 
 
-    def __modify_user(self, lender: str, borrower: str, amount: float) -> dict:
-        # This is a remodeling of the method I did, but this time assuming debt is a single dictionary.
-        user = lender 
-        other_guy = borrower 
-        user_data = self.__get_user(user)
+    def __modify_user(self, user: str, other_guy: str, amount: float) -> dict:
+        # This is a remodeling of the method I did, but this time assuming debt is a single dictionary. 
+        user_data = self.database.get(user)
         user_data['balance'] -= amount # A new loan will always decrease your balance, either decreasing borrower by amount or decreasing lender by -amount (which is a net increase)
         current_debt = user_data['owes'].get(other_guy, 0) # The current debt is how much the user is owed by other guy
-        total = current_debt + amount # We add the amount, regardless
-        user_data['owes'][other_guy] = total # We update the dictionary
+        user_data['owes'][other_guy] = current_debt + amount # We update the dictionary, adding the amount to the current debt.
         parsed_user_data = self.parse_database_return(user) # Parse this due to the expected output.
         return parsed_user_data
