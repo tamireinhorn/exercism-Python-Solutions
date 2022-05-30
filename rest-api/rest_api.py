04b906bd83d58b87b36c7ed7688af93b6a841876
tamireinhorn <tamtamireinhorn@gmail.com>
2022-05-31 02:52:37 +0300

    Update rest_api.py

--- rest-api/rest_api.py
+++ rest-api/rest_api.py
@@ -12,11 +12,11 @@ class RestAPI:
         new_database = {}
         if database:
             for entry in database['users']:
-                new_entry = {'owes' : {}}
+                new_entry = {}
                 possible_users = set(entry.get('owes').keys()).union(set(entry.get('owed_by'))) # This gets all unique users in that entry!
                 for user in possible_users:
                     user_amount = entry.get('owes').get(user, 0) - entry.get('owed_by').get(user, 0) # We aggregate the debt of that user in the entry in one dict.
-                    new_entry['owes'][user] = user_amount # We redo the owes dictionary
+                    new_entry[user] = user_amount # We redo the owes dictionary
                 new_database[entry['name']] = new_entry # The database is a dictionary whose keys are the names of the people in it.
         self.database = new_database
 
@@ -30,10 +30,8 @@ class RestAPI:
         else:
             return json.dumps({'users': [i for i in self.database]})
     
-    def post(self, url: str, payload: str =None):
+    def post(self, url: str, payload: str):
         url = _clean_url(url)
-        if not payload:
-            raise ValueError('Incorrect request. A POST method requires a payload.')
         payload = json.loads(payload)
         if url == 'add':
             return self.__add_user(payload)
@@ -43,10 +41,10 @@ class RestAPI:
 
     def __add_user(self, payload: dict) -> str:
         new_user = payload['user']
-        new_user_entry = {'owes': {}}
-        self.database[new_user] = new_user_entry
-        return_new_user_entry = deepcopy(new_user_entry)
+        self.database[new_user] = {}
+        return_new_user_entry = {}
         return_new_user_entry['owed_by'] = {}
+        return_new_user_entry['owes'] = {}
         return_new_user_entry['name'] = new_user
         return_new_user_entry['balance'] = 0.0
         return json.dumps(return_new_user_entry)
@@ -60,18 +58,18 @@ class RestAPI:
 
     def parse_database_return(self, user: str) -> dict: 
         # This method exists because the output expects two dicts, one of owes and other of owed_by
-        copy_user_entry = self.database.get(user)
-        copy_user_entry['balance'] = -sum(copy_user_entry['owes'].values())
-        copy_user_entry['owed_by'] = {user: -amount for user, amount in copy_user_entry['owes'].items() if amount < 0} # Negative debt -> user is OWED BY, goes to new dict for output.
-        copy_user_entry['owes'] = {user: amount for user, amount in copy_user_entry['owes'].items() if amount > 0} # Positive debt in owes -> stays
+        user_entry = self.database.get(user)
+        copy_user_entry = {}
+        copy_user_entry['balance'] = -sum(user_entry.values())
+        copy_user_entry['owed_by'] = {user: -amount for user, amount in user_entry.items() if amount < 0} # Negative debt -> user is OWED BY, goes to new dict for output.
+        copy_user_entry['owes'] = {user: amount for user, amount in user_entry.items() if amount > 0} # Positive debt in owes -> stays
         copy_user_entry['name'] = user
-        
         return copy_user_entry 
 
 
     def __modify_user(self, user: str, other_guy: str, amount: float) -> dict:
         # This is a remodeling of the method I did, but this time assuming debt is a single dictionary. 
         user_data = self.database.get(user)
-        user_data['owes'][other_guy] = user_data['owes'].get(other_guy, 0) + amount # We update the dictionary, adding the amount to the current debt.
+        user_data[other_guy] = user_data.get(other_guy, 0) + amount # We update the dictionary, adding the amount to the current debt.
         parsed_user_data = self.parse_database_return(user) # Parse this due to the expected output.
         return parsed_user_data
