c12a218580c8667f0e32dc7b2488760e1fff8b08
tamireinhorn <tamtamireinhorn@gmail.com>
2022-06-03 22:45:18 +0300

    Update rest_api.py
    Simplify and remove useless overhead

--- rest-api/rest_api.py
+++ rest-api/rest_api.py
@@ -2,27 +2,22 @@ from copy import deepcopy
 import json 
 
 
-def _clean_url(url: str) -> str :
-    return url.removeprefix('/')
-
-
-VALID_GETS = ['users']
+VALID_GETS = ['/users']
 class RestAPI:
     def __init__(self, database: dict=None):
         new_database = {}
         if database:
             for entry in database['users']:
                 new_entry = {}
-                possible_users = set(entry.get('owes').keys()).union(set(entry.get('owed_by'))) # This gets all unique users in that entry!
-                for user in possible_users:
-                    user_amount = entry.get('owes').get(user, 0) - entry.get('owed_by').get(user, 0) # We aggregate the debt of that user in the entry in one dict.
-                    new_entry[user] = user_amount # We redo the owes dictionary
+                for user in entry.get('owes'):
+                    new_entry[user] = entry['owes'][user]
+                for user in entry.get('owed_by'):
+                    new_entry[user] = -entry['owed_by'][user]
                 new_database[entry['name']] = new_entry # The database is a dictionary whose keys are the names of the people in it.
         self.database = new_database
 
     def get(self, url: str, payload: dict=None):
-        clean_url = _clean_url(url)
-        if clean_url not in VALID_GETS:
+        if url not in VALID_GETS:
              raise ValueError(f'Invalid request. GET only accepts the following requests: {VALID_GETS}.')
         if payload:
             payload = json.loads(payload)
@@ -31,11 +26,10 @@ class RestAPI:
             return json.dumps({'users': list(self.database)})
     
     def post(self, url: str, payload: str):
-        url = _clean_url(url)
         payload = json.loads(payload)
-        if url == 'add':
+        if url == '/add':
             return self.__add_user(payload)
-        if url == 'iou':
+        if url == '/iou':
             return self.__add_iou(payload)
         raise ValueError('Incorrect request passed to API. Valid POST methods are add, iou.')
 
@@ -55,10 +49,12 @@ class RestAPI:
     def parse_database_return(self, user: str) -> dict: 
         # This method exists because the output expects two dicts, one of owes and other of owed_by
         user_entry = self.database.get(user)
-        copy_user_entry = {'balance':-sum(user_entry.values())}
-        copy_user_entry['owed_by'] = {user: -amount for user, amount in user_entry.items() if amount < 0} # Negative debt -> user is OWED BY, goes to new dict for output.
-        copy_user_entry['owes'] = {user: amount for user, amount in user_entry.items() if amount > 0} # Positive debt in owes -> stays
-        copy_user_entry['name'] = user
+        copy_user_entry = {
+                        'name': user,
+                        'balance': -sum(user_entry.values()),
+                        'owed_by': {user: -amount for user, amount in user_entry.items() if amount < 0},
+                        'owes': {user: amount for user, amount in user_entry.items() if amount > 0}
+                        }
         return copy_user_entry 
 
     def __modify_user(self, user: str, other_guy: str, amount: float) -> dict:
