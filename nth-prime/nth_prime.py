11336a0eedeee6a1da55a4e50ad66e38198904ee
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-06-10 22:18:13 +0300

    Nth prime and Sieve
    Solved both using the sieve algorithm

---
+++ nth-prime/nth_prime.py
@@ -0,0 +1,24 @@
+import math
+
+
+def primes(limit):
+    avaliable_primes = [True for _ in range(limit+1)]
+    prime_candidate = 2
+    while prime_candidate < math.sqrt(limit):
+        if avaliable_primes[prime_candidate]:
+            for j in list(range(prime_candidate, limit+1, prime_candidate))[1:]:
+                avaliable_primes[j] = False
+        prime_candidate += 1
+    return [i for i in range(2, limit+1) if avaliable_primes[i]]
+
+
+def prime(number):
+    if number == 0:
+        raise ValueError("there is no zeroth prime")
+    size = 2 * number
+    sieve = primes(size)
+    while len(sieve) < number:
+        size += number
+        sieve = primes(size)
+    return sieve[number-1]
+    
---
+++ sieve/sieve.py
@@ -0,0 +1,12 @@
+import math 
+
+
+def primes(limit):
+    avaliable_primes = [True for _ in range(limit+1)]
+    prime_candidate = 2
+    while prime_candidate < math.sqrt(limit):
+        if avaliable_primes[prime_candidate]:
+            for j in list(range(prime_candidate, limit+1, prime_candidate))[1:]:
+                avaliable_primes[j] = False
+        prime_candidate += 1
+    return [i for i in range(2, limit+1) if avaliable_primes[i]]
