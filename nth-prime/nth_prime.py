e16c2962d8c73206eb09d0af9898229683ea83f1
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-06-11 05:51:55 +0300

    Update nth_prime.py
    Small optimization using some basic math

--- nth-prime/nth_prime.py
+++ nth-prime/nth_prime.py
@@ -6,7 +6,7 @@ def primes(limit):
     prime_candidate = 2
     while prime_candidate < math.sqrt(limit):
         if avaliable_primes[prime_candidate]:
-            for j in list(range(prime_candidate, limit+1, prime_candidate))[1:]:
+            for j in range(prime_candidate * 2, limit+1, prime_candidate):
                 avaliable_primes[j] = False
         prime_candidate += 1
     return [i for i in range(2, limit+1) if avaliable_primes[i]]
