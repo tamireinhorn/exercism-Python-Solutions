80cf44cbebc9bf01555d5566a8426c94ac2095ea
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-06-13 14:21:43 +0300

    Update binary_search_tree.py
    Properly applied recursion for insertion

--- binary-search-tree/binary_search_tree.py
+++ binary-search-tree/binary_search_tree.py
@@ -29,21 +29,22 @@ class BinarySearchTree:
         return stack
 
     def insert_data(self, value_to_insert):
-        new_node = TreeNode(value_to_insert)
-        if not self.root: # If there is no root, we just start the tree with a node.
-            self.root = new_node
-            return 
-            # So if there is already a root, what do we do?
-        self.recursive_method(value_to_insert, self.root)
-        
-    def recursive_method(self, value_to_insert, current_node: TreeNode):
-        if value_to_insert > current_node.data: # We got to go right.
-            if not current_node.right:
-                current_node.right = TreeNode(value_to_insert) 
-            else:
-                self.recursive_method(value_to_insert, current_node.right) # Bubble down right
+        self.root = self.recursive_method(value_to_insert, self.root)
+
+    def recursive_method(self, value, node: TreeNode):
+        # This method essentially bubbles down following the standard rules, until it finds a leaf node, then places the new node properly.
+        # Then, the recursion back needs that we return the current node.
+        # Imagine you already have a tree like:
+        # 2 <- 4 -> 6
+        # And now you need to insert 1, which would be to the left of 2, naturally. 
+        # So you start the method at the root (4), go down to the else, call the method again. Now node is 2, and we go to the else again.
+        # Since it's left is null, we go return TreeNode(1), which recurses up, making node 2's left that Node(1). HOWEVER, if it just stopped here...
+        # 4 would have lost its left. So after we did 1 <- 2, we need to return 2 because it will recurse up to make 2 <- 4, therefore maintaining the tree.
+        # This is, in a nutshell, why you need the last return. 
+        if not node: # Once we find a leaf node, we return a created node. 
+            return TreeNode(value)
+        if value > node.data:
+            node.right = self.recursive_method(value, node.right) # Bubble down one level. 
         else:
-            if not current_node.left:
-                current_node.left = TreeNode(value_to_insert)
-            else:
-                self.recursive_method(value_to_insert, current_node.left) # Bubble down left
+            node.left = self.recursive_method(value, node.left)
+        return node # You always need to return the current node so that when you bubble down the previous nodes remain in place.
