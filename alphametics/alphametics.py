bdbaa2961f1e7ecb998352c9dd7412d785cc65b5
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-06-20 22:22:28 +0300

    Permutation solution
    Why go the long mile to write incredibly complex solutions if this works

--- alphametics/alphametics.py
+++ alphametics/alphametics.py
@@ -1,29 +1,36 @@
-from copy import deepcopy
-from itertools import zip_longest
 import re
+from itertools import permutations
 
+def solve(puzzle: str):
+    elements = re.findall('\w+', puzzle) # Splits the puzzle into all of the elements of the equation.
+    first_letters = set(w[0] for w in elements) # We need to know the first letters, just so we never have them = 0 because 0 leading is forbidden.
+    avaliable_letters = set(''.join(elements)) # We also need to know all possible letters.
+    number_of_letters = len(avaliable_letters)
+    if number_of_letters > 10:
+        return ValueError('Too many different letters, only 10 allowed.')
+    for permutation in permutations(range(10), number_of_letters): # We create all possible combinations of values the letters can have.
+        invalid_permutation = False # We can't have 0 leading digit, so we need to check
+        avaliable_letters_dict = {letter: value for letter, value in zip(avaliable_letters, permutation)}
+        for first_letter in first_letters: # If any of the first letters has a value of 0 in the dict.
+            if avaliable_letters_dict[first_letter] == 0:
+                invalid_permutation = True # This is permutation doesn't work, we can skip ahead.
+                break 
+        if not invalid_permutation:
+            new_puzzle = translate(puzzle, avaliable_letters_dict)
+            if eval(new_puzzle): # If the equation produced is true, we've found a solution and can just return it.
+                return avaliable_letters_dict 
+    return None
 
-def solve(puzzle: str) -> dict[str, int]:
-    #   1 
-    #  BB
-    # 1LL
-    # First, we need to split the left and right sides
-    new_puzzle = deepcopy(puzzle)
-    #avaliable_letters = {letter: -1 for letter in set(re.sub('\+|\s|=', '', puzzle))}]
-    avaliable_letters = {}
-    elements = re.findall('\w+', puzzle)
-    sizes = list(map(len, elements))
-    right_side = elements[-1]
-    # Is there carryover to the first digit?
-    if max(sizes[0:len(sizes)-1]) < sizes[-1]:
-        # If there is, since everything is single digit, the first letter of the right side = 1
-        avaliable_letters[right_side[0]] = '1'
-        new_puzzle = re.sub(right_side[0], '1', deepcopy(puzzle))
-        # Due to that carry, we also know that the second digit is 0.
-        avaliable_letters[right_side[1]] = 0
-        new_puzzle = re.sub(right_side[1], '0', new_puzzle)
-    return avaliable_letters
+def translate(string: str, dictionary: dict[str, any]) -> str:
+    """Helper function to replace characters in a string with their value in a dictionary mapping.
 
-
-def equation_creation(elements: list[str]):
-    pass
+    Args:
+        string (str): String to be translated.
+        dictionary (dict[str, any]): Dictionary containing the mapping between letters and their values
+    Returns:
+        new_string (str): String with new values.
+    """
+    new_string = string
+    for key, value in dictionary.items():
+        new_string = new_string.replace(key, str(value))
+    return new_string
