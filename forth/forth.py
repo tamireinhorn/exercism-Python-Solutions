b867c23e5efa03ab5e7e96cf50bd00d461d7a990
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-06-28 14:20:27 +0300

    Good progress

--- forth/forth.py
+++ forth/forth.py
@@ -1,15 +1,36 @@
+from copy import copy
+
+
 class StackUnderflowError(Exception):
     def __init__(self, message: str) -> None:
         self.message = message
 
 SUPPORTED_OPERANDS = ['+', '-', '/', '*']
 
-
-def operate(operator: str, stack: list[str]):
+def operate(operator: str, stack: list[str]) -> int:
     try:
         return eval(f'{stack[0]} {operator} {stack[1]}')
     except ZeroDivisionError:
         raise ZeroDivisionError("divide by zero")
+
+
+def _duplicate(stack: list[str]) -> list[str]:
+    return stack + [stack[-1]]
+
+
+def _drop(stack: list[str]) -> list[str]:
+    new_stack = copy(stack)
+    new_stack.pop()
+    return new_stack
+
+
+def _swap(stack: list[str]) -> list[str]:
+    if len(stack) < 2:
+        raise StackUnderflowError("Insufficient number of items in stack")
+    return stack[0:len(stack) -2] + [stack[-1], stack[-2]]
+
+
+WORD_DICT = {'dup': _duplicate, 'drop': _drop, 'swap': _swap}
 
 
 def evaluate(input_data):
@@ -22,4 +43,8 @@ def evaluate(input_data):
             if len(stack) < 2:
                 raise StackUnderflowError("Insufficient number of items in stack")
             stack = [operate(item, stack)]
+        elif item.lower() in WORD_DICT:
+            if len(stack) == 0:
+                raise StackUnderflowError("Insufficient number of items in stack")
+            stack = WORD_DICT[item.lower()](stack)
     return list(map(int, stack))
