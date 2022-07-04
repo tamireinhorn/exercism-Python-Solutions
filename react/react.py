9d13696381905ede1e69e484ac34f2c359b32831
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-07-05 01:12:13 +0300

    Using decorators in react
    Still gotta document this properly.

--- react/react.py
+++ react/react.py
@@ -2,8 +2,17 @@
 
 class InputCell:
     def __init__(self, initial_value):
-        self.value = initial_value
-
+        self._value = initial_value
+    
+    @property
+    def value(self):
+        print('I got called here')
+        return self._value
+    
+    @value.setter
+    def value(self, new_value):
+        print('Setting new value_')
+        self._value = new_value
 
 class ComputeCell:
     def __init__(self, inputs: list[InputCell], compute_function):
