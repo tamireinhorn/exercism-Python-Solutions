e6b9508db53b332ed1ce5cdd9e19798f085a35c6
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-07-10 00:52:53 +0300

    Recognize individual numbers

--- ocr-numbers/ocr_numbers.py
+++ ocr-numbers/ocr_numbers.py
@@ -1,12 +1,34 @@
-def convert(input_grid):
-    if len(input_grid) % 4 != 0:
-        raise ValueError( "Number of input lines is not a multiple of four")
-    
-    pass
+import enum
 
+
+def convert(input_grid):
+    input_size, remainder = divmod(len(input_grid[0]), 3)
+    number_list = []
+    for index, _ in enumerate(input_size):
+        beg_index = index
+        end_index = index + 3
+        number = [input_grid[0][0:3], input_grid[1][0:3], input_grid[2][0:3], input_grid[3][0:3]]
+        number_list.append(number)
+    return recognize_number(input_grid)
+
+ZERO = [" _ ", "| |", "|_|", "   "]
+ONE = ["   ", "  |", "  |", "   "]
+TWO = [" _ ", " _|", "|_ ", "   "]
+THREE = [" _ ", " _|", " _|", "   "]
+FOUR = ["   ", "|_|", "  |", "   "]
+FIVE = [" _ ", "|_ ", " _|", "   "]
+SIX = [" _ ", "|_ ", "|_|", "   "]
+SEVEN = [" _ ", "  |", "  |", "   "]
+EIGHT = [" _ ", "|_|", "|_|", "   "]
+NINE = [" _ ", "|_|", " _|", "   "]
+
+NUMBERS_DICT = {tuple(ZERO): '0', tuple(ONE): '1', tuple(TWO): '2',
+                tuple(THREE): '3', tuple(FOUR): '4', tuple(FIVE): '5',
+                tuple(SIX): '6', tuple(SEVEN): '7', tuple(EIGHT): '8', tuple(NINE): '9'}
 
 def recognize_number(grid):
-    pass
+    return NUMBERS_DICT.get(tuple(grid), '?')
 # My solution idea is to separate in grids of 3x4, match them to numbers, so we would have like 2 functions. 
+
 
 
