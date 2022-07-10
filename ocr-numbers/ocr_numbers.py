b53c492c298198b2c7aed2a7484a643f0d3fa530
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-07-10 18:40:54 +0300

    Solved

--- ocr-numbers/ocr_numbers.py
+++ ocr-numbers/ocr_numbers.py
@@ -1,16 +1,3 @@
-import enum
-
-
-def convert(input_grid):
-    input_size, remainder = divmod(len(input_grid[0]), 3)
-    number_list = []
-    for index, _ in enumerate(input_size):
-        beg_index = index
-        end_index = index + 3
-        number = [input_grid[0][0:3], input_grid[1][0:3], input_grid[2][0:3], input_grid[3][0:3]]
-        number_list.append(number)
-    return recognize_number(input_grid)
-
 ZERO = [" _ ", "| |", "|_|", "   "]
 ONE = ["   ", "  |", "  |", "   "]
 TWO = [" _ ", " _|", "|_ ", "   "]
@@ -26,9 +13,30 @@ NUMBERS_DICT = {tuple(ZERO): '0', tuple(ONE): '1', tuple(TWO): '2',
                 tuple(THREE): '3', tuple(FOUR): '4', tuple(FIVE): '5',
                 tuple(SIX): '6', tuple(SEVEN): '7', tuple(EIGHT): '8', tuple(NINE): '9'}
 
-def recognize_number(grid):
-    return NUMBERS_DICT.get(tuple(grid), '?')
-# My solution idea is to separate in grids of 3x4, match them to numbers, so we would have like 2 functions. 
+# Break down a grid into grids of 4 lines, then process them as 4x3 of numbers
+def regular_grid(input_grid: list[str]) -> str:
+    input_size, remainder = divmod(len(input_grid[0]), 3)
+    if remainder > 0:
+        raise ValueError("Number of input columns is not a multiple of three")
+    answer = ""
+    for index in range(input_size):
+        beg_index = index * 3 
+        end_index = beg_index + 3
+        number = [input_grid[0][beg_index:end_index], 
+                input_grid[1][beg_index:end_index], 
+                input_grid[2][beg_index:end_index], 
+                input_grid[3][beg_index:end_index]]
+        answer += NUMBERS_DICT.get(tuple(number), '?')
+    return answer
 
 
-
+def convert(grid: list[str]) -> str:
+    answer = []
+    lines, remainder = divmod(len(grid), 4)
+    if remainder > 0:
+        raise ValueError("Number of input lines is not a multiple of four")           
+    for index in range(lines):
+        beg = index * 4
+        end = beg + 4
+        answer.append(regular_grid(grid[beg:end]))
+    return ','.join(answer)
