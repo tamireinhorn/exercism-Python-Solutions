8ce7034e3f61963ad880bd75ec6aa1542fab675d
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-07-12 21:34:29 +0300

    More structure for food chain exercise

--- food-chain/food_chain.py
+++ food-chain/food_chain.py
@@ -1,10 +1,13 @@
 BASIS = "I know an old lady who swallowed a"
 END =  "I don't know why she swallowed the fly. Perhaps she'll die."
-ANIMALS = ["fly"]
+ANIMALS = ["fly", "spider"]
+ADDENDUM = ["", "It wriggled and jiggled and tickled inside her.", "How absurd to swallow a bird!"]
 
 
 def generate_verse(verse_number: int):
     verse = [f"{BASIS} {ANIMALS[verse_number-1]}."]
+    if verse_number > 1:
+        verse.append(ADDENDUM[verse_number-1])
     verse.append(END)
     return verse 
 
