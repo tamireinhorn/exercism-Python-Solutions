c5a43fa51e2970f5f04249949e28ada6f3900975
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-07-14 21:30:12 +0300

    Almost solved food chain

--- food-chain/food_chain.py
+++ food-chain/food_chain.py
@@ -1,13 +1,22 @@
 BASIS = "I know an old lady who swallowed a"
 END =  "I don't know why she swallowed the fly. Perhaps she'll die."
-ANIMALS = ["fly", "spider"]
-ADDENDUM = ["", "It wriggled and jiggled and tickled inside her.", "How absurd to swallow a bird!"]
-
-
+ANIMALS = ["fly", "spider", "bird", "cat", "dog", "goat", "cow"]
+SPIDER_EXTRA = "wriggled and jiggled and tickled inside her"
+ADDENDUM = ["", f"It {SPIDER_EXTRA}.", "How absurd to swallow a bird!", 
+            "Imagine that, to swallow a cat!", "What a hog, to swallow a dog!", "Just opened her throat and swallowed a goat!",
+             "I don't know how she swallowed a cow!"]
+ 
 def generate_verse(verse_number: int):
-    verse = [f"{BASIS} {ANIMALS[verse_number-1]}."]
+    verse_animal = ANIMALS[verse_number-1]
+    verse = [f"{BASIS} {verse_animal}."]
     if verse_number > 1:
         verse.append(ADDENDUM[verse_number-1])
+        reverse_animals = ANIMALS[:verse_number][::-1]
+        short_reverse_animals = reverse_animals[1:]
+        for animal, next_animal in zip(reverse_animals, short_reverse_animals):
+            if next_animal == 'spider':
+                next_animal = f"{next_animal} that {SPIDER_EXTRA}"
+            verse.append(f"She swallowed the {animal} to catch the {next_animal}.")
     verse.append(END)
     return verse 
 
