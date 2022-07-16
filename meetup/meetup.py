b5006a776092550d0fdd927dcb3e4337e8267b25
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-07-16 15:26:09 +0300

    Solved meetup

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 import calendar
 # subclassing the built-in ValueError to create MeetupDayException
 class MeetupDayException(ValueError):
@@ -11,24 +11,43 @@ class MeetupDayException(ValueError):
         self.message = message
 
 
-def meetup(year, month, week, day_of_week):
-    week_matching = {"first": 1, "second": 2, "third": 3, "fourth": 4}
-    desired_counter = -1
-    if week in week_matching:
-        desired_counter = week_matching.get(week)
-    elif week == 'teenth':
-        pass
-    c = calendar.Calendar()
+def _check_calendar_day(calendar_day: date, day_of_week: str, month: int):
+    return calendar_day.strftime('%A') == day_of_week and calendar_day.month == month
+
+
+def _ordinal_meetup(week_calendar: list[list[date]], day_of_week: str, month: int, desired_counter: int):
     day_counter = 0
-    current_answer = ''
-    week_calendar = c.monthdatescalendar(year, month)
     for calendar_week in week_calendar:
         for calendar_day in calendar_week:
             if day_counter == desired_counter:
                 return current_answer
-            if calendar_day.strftime('%A') == day_of_week and calendar_day.month == month: # FIRST
+            if _check_calendar_day(calendar_day, day_of_week, month): # FIRST
                 day_counter += 1
                 current_answer = calendar_day
-            
-# Look into the datetime module cause it probably gives us everything
-# note that teenth will be the only weird Day of Week. 
+    raise MeetupDayException("That day does not exist.")
+
+
+def _teenth(week_calendar: list[list[date]], day_of_week: str, month: int):
+    for calendar_week in week_calendar:
+            for calendar_day in calendar_week:
+                if calendar_day.day >= 13 and _check_calendar_day(calendar_day, day_of_week, month):
+                    return calendar_day
+
+
+def _last(week_calendar: list[list[date]], day_of_week: str, month: int):
+    for calendar_week in week_calendar[::-1]:
+        for calendar_day in calendar_week:
+            if _check_calendar_day(calendar_day, day_of_week, month):
+                return calendar_day
+
+
+def meetup(year: int, month: int, week: str, day_of_week: str):
+    c = calendar.Calendar()
+    week_matching = {"first": 1, "second": 2, "third": 3, "fourth": 4, "fifth": 5}
+    week_calendar = c.monthdatescalendar(year, month)
+    if week in week_matching:
+        return _ordinal_meetup(week_calendar, day_of_week, month, week_matching.get(week))
+    elif week == 'teenth':
+        return _teenth(week_calendar, day_of_week, month)
+    elif week == 'last':
+        return _last(week_calendar, day_of_week, month)
