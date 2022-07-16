cc7027860521c19ff9ea770e10c1226c21dd5169
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-07-16 14:20:26 +0300

    Update meetup.py
    57 tests are now working

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -1,4 +1,5 @@
 from datetime import date
+import calendar
 # subclassing the built-in ValueError to create MeetupDayException
 class MeetupDayException(ValueError):
     """Exception raised when the Meetup weekday and count do not result in a valid date.
@@ -11,6 +12,23 @@ class MeetupDayException(ValueError):
 
 
 def meetup(year, month, week, day_of_week):
-    pass
+    week_matching = {"first": 1, "second": 2, "third": 3, "fourth": 4}
+    desired_counter = -1
+    if week in week_matching:
+        desired_counter = week_matching.get(week)
+    elif week == 'teenth':
+        pass
+    c = calendar.Calendar()
+    day_counter = 0
+    current_answer = ''
+    week_calendar = c.monthdatescalendar(year, month)
+    for calendar_week in week_calendar:
+        for calendar_day in calendar_week:
+            if day_counter == desired_counter:
+                return current_answer
+            if calendar_day.strftime('%A') == day_of_week and calendar_day.month == month: # FIRST
+                day_counter += 1
+                current_answer = calendar_day
+            
 # Look into the datetime module cause it probably gives us everything
 # note that teenth will be the only weird Day of Week. 
