dc2df13874f671c589097c67454b5fcf8f74e57d
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-07-18 23:05:39 +0300

    Initial 3 tests working

---
+++ dominoes/dominoes.py
@@ -0,0 +1,17 @@
+from itertools import chain
+
+
+def can_chain(dominoes: list[tuple[int, int]]):
+    # To verify chains, we need to have a base case (because this will likely be recursive)
+    return _recursive_chain(dominoes)
+    pass
+
+def _recursive_chain(dominoes: list[tuple[int, int]]):
+    if len(dominoes) == 0:
+        return dominoes
+    if len(dominoes) == 1:
+        chainable = dominoes[0][0] == dominoes[0][1]
+        if chainable:
+            return dominoes
+        return 
+    return 1
