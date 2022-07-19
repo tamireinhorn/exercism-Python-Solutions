cb88cf9d498d75f3e0888baa5e71250816f059fd
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-07-20 02:28:16 +0300

    Update dominoes.py
    Moved chaining verification to a function for a pair of dominoes. Seems like the best option so far

--- dominoes/dominoes.py
+++ dominoes/dominoes.py
@@ -1,5 +1,4 @@
-from itertools import chain
-
+from itertools import permutations
 
 def can_chain(dominoes: list[tuple[int, int]]):
     # To verify chains, we need to have a base case (because this will likely be recursive)
@@ -10,8 +9,18 @@ def _recursive_chain(dominoes: list[tuple[int, int]]):
     if len(dominoes) == 0:
         return dominoes
     if len(dominoes) == 1:
-        chainable = dominoes[0][0] == dominoes[0][1] # TODO, we could compare two dominoes, by looking at all 4 combinations and see if we can chain them.
-        if chainable:
-            return dominoes
-        return 
+        return check_chainability(dominoes[0], dominoes[0])
     return 1
+
+
+def check_chainability(domino_1: tuple[int, int], domino_2: tuple[int, int]):
+    if domino_1 == domino_2:
+        if domino_1[0] == domino_1[1]:
+            return [domino_1]
+        return None
+    for domino_1_possibility in list(permutations(domino_1)):
+        for domino_2_possibility in list(permutations(domino_2)):
+            if domino_1_possibility[0] == domino_2_possibility[0]:
+                if domino_1 == domino_2:
+                    return [domino_1]
+                return [domino_1_possibility, domino_2_possibility]
