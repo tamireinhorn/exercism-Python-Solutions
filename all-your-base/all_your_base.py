297ea6be2eb74b98aa06d38802955cf8767f215f
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-07-26 18:04:20 +0300

    Solved all your base

--- all-your-base/all_your_base.py
+++ all-your-base/all_your_base.py
@@ -1,13 +1,19 @@
+# Math based on :https://www.mathsisfun.com/base-conversion-method.html
 def rebase(input_base: int, digits: list[int], output_base: int) -> list[int]:
     if input_base < 2:
         raise ValueError("input base must be >= 2")
     if output_base < 2:
         raise ValueError("output base must be >= 2")
-    number = sum(digit * (input_base ** power) for digit, power in zip(digits[::-1], range(len(digits))))
+    number = 0
+    for digit, power in zip(digits[::-1], range(len(digits))):
+        if digit < 0 or digit >= input_base:
+            raise ValueError("all digits must satisfy 0 <= d < input base")
+        number += digit * (input_base ** power)
     quotient, remainder = divmod(number, output_base)
     answer = [remainder]
-    while quotient > output_base:
+    while quotient >= output_base:
         quotient, remainder = divmod(quotient, output_base)
-        print(2)
-    answer.append(quotient)
+        answer.append(remainder)
+    if quotient != 0:
+        answer.append(quotient)
     return answer[::-1]
