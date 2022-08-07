7040e9e9c0af19fa8a9197e571d652073d516ea1
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-08-07 16:08:45 +0300

    Very close to solving

--- rectangles/rectangles.py
+++ rectangles/rectangles.py
@@ -1,44 +1,50 @@
-
 from collections import namedtuple
 import re
 
 
 def rectangles(strings: list[str]) -> int:
-    Edge = namedtuple('Edge', ['row', 'start', 'end'])
+    Edge = namedtuple("Edge", ["row", "start", "end"])
     true_counter = 0
-    edge_pattern = re.compile("\+-*(?=\+)") # An edge is a + sign, followed by maybe a few -, and then finished by a +.
+    edge_pattern = re.compile(
+        "\+-*(?=\+)"
+    )  # An edge is a + sign, followed by maybe a few -, and then finished by a +.
+    border_pattern = re.compile(
+        "\+\|*(?=\+)"
+    )  # A border is a + sign, followed by maybe a few -, and then finished by a +.
+    transposed = ["".join(i) for i in map(list, zip(*strings))]
+    # For every pair of corners of a rectangle, we need to make sure that the lines below it are either + or |.
     all_corners = []
+    all_borders = []
     for index, row in enumerate(strings):
         edges = list(re.finditer(edge_pattern, row))
         row_corners = [Edge(index, i.start(), i.end()) for i in edges]
         if len(edges) > 1:
-            long_edge = (Edge(index, edges[0].start(), edges[-1].end()))
+            long_edge = Edge(index, edges[0].start(), edges[-1].end())
             row_corners.append(long_edge)
-        all_corners.append(row_corners)
-    # Completing rectangles:
-    # For every pair of corners of a rectangle, we need to make sure that the lines below it are either + or |.
-    # This seems to point to us needing the height as well. List of lists does.
-    # transposed = list(map(list, zip(*strings)))
-    # for row_index, corner_list in enumerate(all_corners):
-    #     for corner in corner_list: # The problem here is the assumption that a single corner will be one rectangle, which with sharing is false.
-    #         # I guess a simple solution would be to iterate over all possible '+' edges below it???
-    #         start_column = "".join(transposed[corner[0]][row_index + 1 :])
-    #         stoppage_start_list = re.finditer("\+", start_column)
-    #         end_column = "".join(transposed[corner[1]][row_index + 1 :])
-    #         stoppage_end_list = re.finditer("\+", end_column)
-    #         for stoppage_start_column, stoppage_end_column in zip(stoppage_start_list, stoppage_end_list):
-    #             stoppage_start_column, stoppage_end_column = stoppage_start_column.start(), stoppage_end_column.start() 
-    #             start_set = set(start_column[:stoppage_start_column+1])
-    #             end_set = set(end_column[:stoppage_end_column+1])
-    #             full_set = start_set.union(end_set)
-    #             print(2)
-    #             if (
-    #                 '-' not in full_set
-    #                 and ' ' not in full_set
-    #                 and stoppage_end_column >= 0
-    #                 and stoppage_start_column >= 0
-    #                 and stoppage_start_column == stoppage_end_column
-    #             ):
-    #                 true_counter += 1
-    #         # Everything below the edge.
+        all_corners += row_corners
+    for index, row in enumerate(transposed):
+        borders = list(re.finditer(border_pattern, row))
+        column_corners = [Edge(index, i.start(), i.end()) for i in borders]
+        if len(borders) > 1:
+            long_border = Edge(index, borders[0].start(), borders[-1].end())
+            column_corners.append(long_border)
+        all_borders += column_corners
+    for edge in all_corners:
+        # Get all candidates to be the closing part of the rectangle:
+        lower_edges = filter(
+            lambda e: e.row > edge.row and e.start == edge.start and e.end == edge.end,
+            all_corners,
+        )
+        for lower_edge in lower_edges:
+            qualifying_borders = list(
+                filter(
+                    lambda b: ((b.row == edge.start and b.row == lower_edge.start ) or (b.row == edge.end 
+                    and b.row == lower_edge.end)) and b.start == edge.row and b.end == lower_edge.row,
+                    all_borders,
+                
+            ))
+            if len(qualifying_borders) == 2:
+                true_counter += 1
+            print(lower_edge)
+        print(edge)
     return true_counter
