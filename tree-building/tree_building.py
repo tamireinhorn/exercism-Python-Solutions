d84fafc12418229c4734dab784e9f18466b3d72b
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-08-11 04:05:19 +0300

    Update tree_building.py
    Less loops used, more enumerates and better logic

--- tree-building/tree_building.py
+++ tree-building/tree_building.py
@@ -21,21 +21,18 @@ def BuildTree(records: list[Record]):
         raise ValueError("Record id is invalid or out of order.")
     trees = []
     parent = {}
-    for i, j in zip(range(len(ordered_id)), records): # Updated this to a zip. This ONLY serves for errors, and then creating the list of Nodes of ordered ids.
-            if j.record_id < j.parent_id: # The error message in the exercise is really weird. The record id is ALWAYS larger than parent, except root.
+    for index, record in enumerate(records): # Updated this to a zip. This ONLY serves for errors, and then creating the list of Nodes of ordered ids.
+            if record.record_id < record.parent_id: # The error message in the exercise is really weird. The record id is ALWAYS larger than parent, except root.
                 raise ValueError("Node record_id should be smaller than it's parent_id.")
-            if j.record_id == j.parent_id and j.record_id != 0:
+            if record.record_id == record.parent_id and record.record_id != 0:
                 raise ValueError("Only root should have equal record and parent id.")
-            trees.append(Node(ordered_id[i]))
-    for i in range(len(ordered_id)): # Iterate over the range of ids.
-        for j in trees: # Then, iterate over the list of nodes.
-            if i == j.node_id: # If they are the same, the parent variable is the j-th Node.
-                parent = j
-        for j in records: # Now, iterate over the sorted records.
-            if j.parent_id == i: # If we are on i == j, then:
-                for k in trees: # Iterate over fucking trees again
-                    if j.record_id == k.node_id and k.node_id != 0: # If it's not the root and the current ordered record's id is the same as the Node, then that's the child.
-                        child = k
+            trees.append(Node(ordered_id[index]))
+    for index, parent in enumerate(trees): # Iterate over the range of ids.
+        for record in records: # Now, iterate over the sorted records.
+            if record.parent_id == index: # If we are on i == j, then:
+                for tree in trees: # Iterate over fucking trees again
+                    if record.record_id == tree.node_id and tree.node_id != 0: # If it's not the root and the current ordered record's id is the same as the Node, then that's the child.
+                        child = tree
                         parent.children.append(child) # You get that node, get the list of it's children then add the k-th Node to it. 
     return trees[0]
 
