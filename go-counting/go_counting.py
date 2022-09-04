189003ca23a4effc576e45a45916635eec3f823e
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-09-04 18:09:26 +0300

    Go counting start
    Exceptions already in place

---
+++ go-counting/go_counting.py
@@ -0,0 +1,46 @@
+BLACK = "B"
+WHITE = "W"
+NONE = ""
+
+class Board:
+    """Count territories of each player in a Go game
+
+    Args:
+        board (list[str]): A two-dimensional Go board
+    """
+
+    def __init__(self, board):
+        self.board = board
+        self._board_height = len(board) -1
+        self._board_width = len(board[0]) -1
+
+    def territory(self, x, y):
+        """Find the owner and the territories given a coordinate on
+           the board
+
+        Args:
+            x (int): Column on the board
+            y (int): Row on the board
+
+        Returns:
+            (str, set): A tuple, the first element being the owner
+                        of that area.  One of "W", "B", "".  The
+                        second being a set of coordinates, representing
+                        the owner's territories.
+        """
+        if y < 0 or x < 0 or y > self._board_height or x > self._board_width:
+            raise ValueError("Invalid coordinate")
+        pass
+
+    def territories(self):
+        """Find the owners and the territories of the whole board
+
+        Args:
+            none
+
+        Returns:
+            dict(str, set): A dictionary whose key being the owner
+                        , i.e. "W", "B", "".  The value being a set
+                        of coordinates owned by the owner.
+        """
+        pass
