ff26e05cb988b8025f799946abeddac78224bc71
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-09-05 11:33:46 +0300

    Update go_counting.py
    Create neighbor method to get all adjacent tiles.

--- go-counting/go_counting.py
+++ go-counting/go_counting.py
@@ -13,6 +13,8 @@ class Board:
         self.board = board
         self._board_height = len(board) -1
         self._board_width = len(board[0]) -1
+        self._black_territories = []
+        self._white_territories = []
 
     def territory(self, x, y):
         """Find the owner and the territories given a coordinate on
@@ -30,7 +32,10 @@ class Board:
         """
         if y < 0 or x < 0 or y > self._board_height or x > self._board_width:
             raise ValueError("Invalid coordinate")
-        pass
+        board = self.board
+        examined_terr = board[y][x]
+        neighbors = self.get_neighbors(x, y)
+        return None, {}
 
     def territories(self):
         """Find the owners and the territories of the whole board
@@ -44,3 +49,24 @@ class Board:
                         of coordinates owned by the owner.
         """
         pass
+    
+    def get_neighbors(self, x: int, y: int) -> list[tuple[int, int]]:
+        """Find all the neighboring territories of a defined coordinate.
+
+        Args:
+            x (int): Column on the board
+            y (int): Row on the board
+
+        Returns:
+            list[tuple[int, int]]: List of tuples defining (column, row) on the board for the territories neighboring the specified one.
+        """
+        neighbors = []
+        if x != self._board_width:
+            neighbors.append((x+1, y))
+        if x != 0:
+            neighbors.append((x-1, y))
+        if y != self._board_height:
+            neighbors.append((x, y+1))
+        if y != 0:
+            neighbors.append((x, y-1))
+        return neighbors
