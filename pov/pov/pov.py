ce56a039f82297cc504f9a45e6a2c39fb47d1b0e
Tamir <37386851+tamireinhorn@users.noreply.github.com>
2022-11-03 22:31:35 +0200

    Update pov.py
    Basic tests

--- pov/pov/pov.py
+++ pov/pov/pov.py
@@ -19,7 +19,28 @@ class Tree:
         return self.__dict__() == other.__dict__()
 
     def from_pov(self, from_node):
-        pass
+        path = self.path_from_self(from_node)
+        if not path: # If the path is empty, there is no possible path to it and thus no reorientation.
+            raise ValueError('Tree could not be reoriented')
+        return path[-1] # The last node in path is the from node where the new root is.
+
 
     def path_to(self, from_node, to_node):
         pass
+
+    def path_from_self(self, to_node: str) -> list['Tree']:
+        """Builds a path from the root node to a specific node.
+
+        Args:
+            to_node (str): Node where we want to get to.
+
+        Returns:
+            _type_: _description_
+        """
+        if self.label == to_node: # Base case for recursion, we spit out the actual tree itself because we're there.
+            return [self]
+        for child in self.children:
+            temp = child.path_from_self(to_node) # Apply the same method again, recursively
+            if temp:
+                return [self] + temp
+        return [] # Empty otherwise.
